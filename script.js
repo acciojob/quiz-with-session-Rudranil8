34f0f21a0c8ac8f53372d6b289f31e0743f93ffd
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 16:53:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,6 +26,48 @@ const questions = [
   },
 ];
 
+// function renderQuestions() {
+//   for (let i = 0; i < questions.length; i++) {
+//     const question = questions[i];
+//     const questionElement = document.createElement("div");
+//     const questionText = document.createTextNode(question.question);
+//     questionElement.appendChild(questionText);
+//     for (let j = 0; j < question.choices.length; j++) {
+//       const choice = question.choices[j];
+//       const choiceElement = document.createElement("input");
+//       choiceElement.setAttribute("type", "radio");
+//       choiceElement.setAttribute("name", `question-${i}`);
+//       choiceElement.setAttribute("value", choice);
+//       if (userAnswers[i] === choice) {
+//         choiceElement.setAttribute("checked", true);
+//       }
+//       const choiceText = document.createTextNode(choice);
+//       questionElement.appendChild(choiceElement);
+//       questionElement.appendChild(choiceText);
+//     }
+//     questionsElement.appendChild(questionElement);
+//   }
+// }
+// renderQuestions();
+// // starts here
+// const questionsElement = document.getElementById('questions');
+// choiceElement.addEventListener('change', ((i, choice) => {
+//   return function() {
+//     userAnswers[i] = choice;
+//     sessionStorage.setItem('progress', JSON.stringify(userAnswers));
+//   }
+// })(i, choice));
+// document.getElementById('submit').addEventListener('click', function() {
+//   let score = 0;
+//   for (let i = 0; i < questions.length; i++) {
+//     if (userAnswers[i] === questions[i].answer) {
+//       score++;
+//     }
+//   }
+//   document.getElementById('score').innerText = 'Your score is ' + score + ' out of ' + questions.length + '.';
+//   localStorage.setItem('score', score);
+// });
+
 function renderQuestions() {
   for (let i = 0; i < questions.length; i++) {
     const question = questions[i];
@@ -44,19 +86,23 @@ function renderQuestions() {
       const choiceText = document.createTextNode(choice);
       questionElement.appendChild(choiceElement);
       questionElement.appendChild(choiceText);
+
+      // Move the event listener here
+      choiceElement.addEventListener('change', ((i, choice) => {
+        return function() {
+          userAnswers[i] = choice;
+          sessionStorage.setItem('progress', JSON.stringify(userAnswers));
+        }
+      })(i, choice));
     }
     questionsElement.appendChild(questionElement);
   }
 }
-renderQuestions();
-// starts here
+
 const questionsElement = document.getElementById('questions');
-choiceElement.addEventListener('change', ((i, choice) => {
-  return function() {
-    userAnswers[i] = choice;
-    sessionStorage.setItem('progress', JSON.stringify(userAnswers));
-  }
-})(i, choice));
+let userAnswers = JSON.parse(sessionStorage.getItem('progress')) || [];
+renderQuestions();
+
 document.getElementById('submit').addEventListener('click', function() {
   let score = 0;
   for (let i = 0; i < questions.length; i++) {
